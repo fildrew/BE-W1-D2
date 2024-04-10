2339d64a7cc05f78003f6231d8626e4bc128f0a5
philip <philip>
2024-04-10 23:19:11 +0200

    Update success.php to display a personalized welcome message

--- sucess.php
+++ sucess.php
@@ -6,6 +6,18 @@
     <title>Success</title>
 </head>
 <body>
+
+    <?php
+    session_start();
+    if (isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+        echo "<h1>Welcome, $username!</h1>";
+    } else {
+        echo "<h1>Welcome, Guest!</h1>";
+    }
+    ?>
+
+    
     <div class="alert alert-info" role="alert">
         ✅Form recieved a successful request.
     </div>
